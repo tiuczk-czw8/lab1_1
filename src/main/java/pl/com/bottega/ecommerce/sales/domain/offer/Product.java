ef43235bad14aa17838b1c25167b4aeae0a6e07d
Marcin Kotulski <233000@edu.p.lodz.pl>
2019-10-31 09:51:53 +0100

    add Product class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -1,4 +1,83 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Product {
+
+    private String productId;
+
+    private String productName;
+
+    private Date productSnapshotDate;
+
+    private String productType;
+
+    private Money price;
+
+    public Product(String productId, String productName, Date productSnapshotDate, String productType, Money price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productSnapshotDate = productSnapshotDate;
+        this.productType = productType;
+        this.price = price;
+    }
+    
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Date getProductSnapshotDate() {
+        return productSnapshotDate;
+    }
+
+    public void setProductSnapshotDate(Date productSnapshotDate) {
+        this.productSnapshotDate = productSnapshotDate;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public void setPrice(Money price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productId, product.productId) &&
+                Objects.equals(productName, product.productName) &&
+                Objects.equals(productSnapshotDate, product.productSnapshotDate) &&
+                Objects.equals(productType, product.productType) &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName, productSnapshotDate, productType, price);
+    }
+
 }
