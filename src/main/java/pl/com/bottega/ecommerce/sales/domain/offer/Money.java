e2ab05e0e153f7ee2ddec97967cea5c4c88241b0
Mark0tny <marcinkotulski95@gmail.com>
2019-11-06 19:38:15 +0100

    Money class refactor

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -43,4 +43,21 @@ public class Money {
     public int hashCode() {
         return Objects.hash(value, currency);
     }
+
+    public boolean sameAs(Money other, double delta) {
+        BigDecimal max;
+        BigDecimal min;
+        if (value.compareTo(other.value) > 0) {
+            max = value;
+            min = other.value;
+        } else {
+            max = other.value;
+            min = value;
+        }
+
+        BigDecimal difference = max.subtract(min);
+        BigDecimal acceptableDelta = max.multiply(BigDecimal.valueOf(delta / 100));
+
+        return acceptableDelta.compareTo(difference) > 0;
+    }
 }
