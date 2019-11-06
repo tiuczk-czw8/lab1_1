584d2bfe382d9ab7d4b6f4e84f308eb732a5fb12
CrossKarol <karolekkawecki@o2.pl>
2019-11-06 21:13:39 +0100

    Updated getters and function in OfferItem

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
@@ -20,36 +20,112 @@ public class OfferItem {
 
     // product
     private Product product;
+
     private int quantity;
+
     private Money totalCost;
+
 
     // discount
     private Money discount;
 
-    public OfferItem(Product product, int quantity) {
-        this(product, quantity, null);
+    private String discountCause;
+
+    public OfferItem(Product product,
+                     int quantity) {
+        this(product, quantity, null, null);
     }
 
-    public OfferItem(Product product, int quantity, Money discount) {
-
+    public OfferItem(Product product,
+                     int quantity, Money discount, String discountCause) {
         this.product = product;
+
         this.quantity = quantity;
         this.discount = discount;
-
-
         BigDecimal discountValue = new BigDecimal(0);
         if (discount != null) {
             discountValue = discountValue.add(discount.getValue());
         }
 
-        this.totalCost.setValue(product.getProductPrice().getValue().multiply(new BigDecimal(quantity)).subtract(discountValue));
+        this.totalCost.setValue(product.getProductPrice().getValue().multiply(new BigDecimal(quantity))
+                .subtract(discountValue));
     }
 
-    public Product getProduct() {
-        return product;
+
+    public Product getProduct() { return product; }
+
+    public Money getTotalCost() {
+        return totalCost;
     }
 
-    public BigDecimal getProductPrice() {
-        return productPrice;
+    public Money getDiscount() {
+        return discount;
     }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getDiscountCause() {return discountCause;}
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( discount,discountCause, product, quantity, totalCost);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        OfferItem other = (OfferItem) obj;
+        return Objects.equals(totalCost, other.totalCost)
+                && Objects.equals(discount, other.discount)
+                && Objects.equals(discountCause, other.discountCause)
+                && Objects.equals(product, other.product)
+                && quantity == other.quantity;
+    }
+
+    /**
+     *
+     * @param item
+     * @param delta
+     *            acceptable percentage difference
+     * @return
+     */
+    public boolean sameAs(OfferItem other, double delta) {
+        if (product == null) {
+            if (other.product != null) {
+                return false;
+            }
+        } else if (!product.equals(other.product)) {
+            return false;
+        }
+
+        if (quantity != other.quantity) {
+            return false;
+        }
+
+        BigDecimal max;
+        BigDecimal min;
+        if (totalCost.getValue().compareTo(other.totalCost.getValue()) > 0) {
+            max = totalCost.getValue();
+            min = other.totalCost.getValue();
+        } else {
+            max = other.totalCost.getValue();
+            min = totalCost.getValue();
+        }
+
+        BigDecimal difference = max.subtract(min);
+        BigDecimal acceptableDelta = max.multiply(BigDecimal.valueOf(delta / 100));
+
+        return acceptableDelta.compareTo(difference) > 0;
+    }
+
 }
